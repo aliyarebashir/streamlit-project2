4b99de5d6c4544f361df43db56efb4ca3163439a
uwes <uwess529300@gmail.com>
2023-09-11 22:42:43 +0300

    changed background color

--- app.py
+++ app.py
@@ -2,9 +2,29 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import numpy as np
+import base64
+
 st.set_page_config(page_icon="bar_chart",
                    page_title="Forbes 2022 Billionaire list Analysis",layout="wide",
                    initial_sidebar_state="collapsed")
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('./images/background.png')
+
 st.title("FORBES 2022 BILLIONAIRES LISTS ANALYSIS")
 df= pd.read_csv("forbes_2022_billionaires.csv")
 df["Networth"] = df["Networth"].apply(lambda x: float(x.split(" ")[0].replace("$"," ").strip()))
@@ -17,11 +37,11 @@ with col1:
                  hover_name="Age")
     fig.update_layout(title=dict(xanchor='center', yanchor="top", x=0.4, y=0.9, font=dict(size=30)),
                       font_color="blueviolet",
-                      legend_title_font_color="Black", title_font_color="lightblue",
+                      legend_title_font_color="red", title_font_color="violet",
                       height=500,width=500)
     fig.update_layout( {'plot_bgcolor': 'rgba(0, 0, 0, 0.2)'})
-    fig.update_yaxes(title="Networth in Billions", title_font_color="blueviolet", tickfont=dict(color="darkblue"))
-    fig.update_xaxes(title="Bilionaires names", title_font_color="blueviolet", tickfont=dict(color="darkblue"))
+    fig.update_yaxes(title="Networth in Billions", title_font_color="blueviolet", tickfont=dict(color="gold"))
+    fig.update_xaxes(title="Bilionaires names", title_font_color="blueviolet", tickfont=dict(color="gold"))
     st.plotly_chart(fig)
     st.write("The oldest billionaire is George Joseph with a networth of 1.8 Billions and is aged 100 years old,fellowed by Robert kuok whose networth is 11.7 Billions and aged 98 years old")
 
@@ -31,12 +51,12 @@ with col2:
     fig = px.bar(top_10, x="Name", y="Networth", color="Age", title="Top 10 Youngest Billionaires", hover_name="Age")
     fig.update_layout(title=dict(xanchor='center', yanchor="top", x=0.4, y=0.9, font=dict(size=30)),
                       font_color="blueviolet",
-                      legend_title_font_color="Black", title_font_color="lightblue",
+                      legend_title_font_color="red", title_font_color="violet",
                       width=500,
                       height=500)
     fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0.2)'})
-    fig.update_yaxes(title="Networth in Billions", title_font_color="blueviolet", tickfont=dict(color="darkblue"))
-    fig.update_xaxes(title="Bilionaires names", title_font_color="blueviolet", tickfont=dict(color="darkblue"))
+    fig.update_yaxes(title="Networth in Billions", title_font_color="blueviolet", tickfont=dict(color="gold"))
+    fig.update_xaxes(title="Bilionaires names", title_font_color="blueviolet", tickfont=dict(color="gold"))
     st.plotly_chart(fig)
     st.write(
         "The Youngest billionaire is Kevin David Lehmann with a networth of 2.4 Billions and is aged 19 years old,fellowed by Pedro Faranceschi whose networth is 1.5 Billions and aged 25 years old")
@@ -49,8 +69,8 @@ fig.update_layout(title=dict(xanchor='center', yanchor="top", x=0.5, y=0.9, font
                   height=600,
                   width=1200)
 fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0.2)'})
-fig.update_xaxes(title_font_color="blueviolet", tickfont=dict(color="darkblue"))
-fig.update_yaxes(title_font_color="blueviolet", tickfont=dict(color="darkblue"))
+fig.update_xaxes(title_font_color="blueviolet", tickfont=dict(color="gold"))
+fig.update_yaxes(title_font_color="blueviolet", tickfont=dict(color="gold"))
 st.plotly_chart(fig)
 st.write("The youngest Billionaire is aged 19 Years old ,while oldest Billionaire is aged 100 Years old")
 st.write("Most of the Billionaires are aged between 50 and 80 years old")
@@ -63,7 +83,7 @@ with col5:
     fig.update_traces(hole=.5, textinfo='percent+label')
     fig.update_layout(
                       font_color="blueviolet",
-                      legend=dict(title="Gender", title_font_color="black"))
+                      legend=dict(title="Gender", title_font_color="red"))
     fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0.2)'})
     st.plotly_chart(fig)
     st.write("There are more Male Billionaires than Female Billionaires")
@@ -79,7 +99,7 @@ with col6:
                        hover_data=['Country', 'Number of females'], color="Country")
     fig.update_layout(
                       font_color="blueviolet",
-                      legend=dict(title="Gender", title_font_color="black"))
+                      legend=dict(title="Gender", title_font_color="red"))
     fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0.2)'})
     fig.update_traces(textposition='inside', textinfo='percent+label')
     st.plotly_chart(fig)
@@ -91,14 +111,14 @@ fig = px.bar(indst_hight_no, x="Industries", y="Total numbers",
                  hover_data=(["Industries", "Total numbers"]))
 fig.update_layout(title=dict(xanchor='center', yanchor="top", x=0.4, y=0.9, font=dict(size=30)),
                   font_color="blue violet",
-                  legend=dict(title_font_color="black", font_color="green"),
+                  legend=dict(title_font_color="red", font_color="violet"),
                   height=600,
                   width=1200
                   )
 fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0.2)'})
 fig.update_yaxes(title="Total number of Billionaires", title_font_color="blueviolet",
-                     tickfont=dict(color="darkblue"))
-fig.update_xaxes(title_font_color="blueviolet", tickfont=dict(color="darkblue"))
+                     tickfont=dict(color="gold"))
+fig.update_xaxes(title_font_color="blueviolet", tickfont=dict(color="gold"))
 st.plotly_chart(fig)
 st.write("Finance and Investments is the industry with highest numbers of Billionaires ,followed by Technology and Manufacturing")
 st.write("Gambling and Casinos is the industry with the least numbers of Billionaires")
@@ -109,14 +129,14 @@ fig=px.line(indt_most_f,x="Industries",y="Number of Females",color="Industries",
             symbol="Industries",title="industries with highest number of female billionaires")
 fig.update_layout(title=dict(xanchor='center',yanchor="top",x=0.5,y=0.9,font=dict(size=30)),
                   title_font_color="lightblue",
-                  legend=dict(title="Gender",title_font_color="black",font_color="green"),
+                  legend=dict(title="Gender",title_font_color="red",font_color="violet"),
                   font_color="blueviolet",
                   width=1200,
                   height=600)
 fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0.2)'})
 fig.update_traces(textposition="middle right")
-fig.update_xaxes(title_font_color="blueviolet",tickfont=dict(color="darkblue"))
-fig.update_yaxes(title_font_color="blueviolet",tickfont=dict(color="darkblue"))
+fig.update_xaxes(title_font_color="blueviolet",tickfont=dict(color="gold"))
+fig.update_yaxes(title_font_color="blueviolet",tickfont=dict(color="gold"))
 st.plotly_chart(fig)
 st.write("Manufacturing is the industry with highest numbers of Female Billionaires,folowed by Food and Beverages")
 st.write("Telecom is the industry with the least numbers of Female Billionaires")
@@ -127,12 +147,12 @@ fig=px.bar(top_10_countries,x='Country',y="Number of Billionaires",
         title="Top 10 countries with highest number of Billionaires",color="Country")
 fig.update_layout(title=dict(xanchor='center', yanchor="top", x=0.4, y=0.9, font=dict(size=30)),
                   font_color="blue violet",
-                  legend=dict(title_font_color="black", font_color="green"),
+                  legend=dict(title_font_color="red", font_color="violet"),
                   height=600,
                   width=1200)
 fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0.2)'})
-fig.update_xaxes(title_font_color="blueviolet",tickfont=dict(color="darkblue"))
-fig.update_yaxes(title_font_color="blueviolet",tickfont=dict(color="darkblue"))
+fig.update_xaxes(title_font_color="blueviolet",tickfont=dict(color="gold"))
+fig.update_yaxes(title_font_color="blueviolet",tickfont=dict(color="gold"))
 st.plotly_chart(fig)
 st.write("United States of America has highest number of Billionaires,Followed by China and then India")
 countries=df.groupby(["Country"]).size().reset_index(name="Number of Billionaires")
